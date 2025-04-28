2713886e976c9b9edad357095a44609600f58908
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-28 10:48:51 -0300

    ajuste para href ser automatizado

--- PortifolioApp/Program.cs
+++ PortifolioApp/Program.cs
@@ -2,14 +2,19 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using PortifolioApp;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Aqui começa o truque:
+var baseElement = builder.HostEnvironment.BaseAddress;
+#if !DEBUG
+    // Em produção (GitHub Pages), altere o base href
+    baseElement = "https://celinhodaltro.github.io/Portifolio/"
+#endif
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
 
-
-builder.Services.AddMudServices();
 await builder.Build().RunAsync();
