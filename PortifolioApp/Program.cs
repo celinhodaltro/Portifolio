2f94984318a9b0416750a5396114e7966184ea5a
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-28 10:37:56 -0300

    voltar

--- PortifolioApp/Program.cs
+++ PortifolioApp/Program.cs
@@ -3,16 +3,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PortifolioApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-// Aqui começa o truque:
-var baseElement = builder.HostEnvironment.BaseAddress;
-#if !DEBUG
-    baseElement = "https://celinhodaltro.github.io/Portifolio/";
-#endif
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
