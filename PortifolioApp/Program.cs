60c26b64a58c97638d10cc507bdbce429df52cee
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-28 10:34:29 -0300

    ajuste href

--- PortifolioApp/Program.cs
+++ PortifolioApp/Program.cs
@@ -3,9 +3,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PortifolioApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Aqui começa o truque:
+var baseElement = builder.HostEnvironment.BaseAddress;
+#if !DEBUG
+    baseElement = "https://celinhodaltro.github.io/Portifolio/";
+#endif
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
 
 await builder.Build().RunAsync();
