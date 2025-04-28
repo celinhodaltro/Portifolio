ffe2a6adcbcbfb838eaa0bb9588ebdda0d2cca88
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-28 17:37:44 -0300

    Criar outro projeto

--- Sources/Application/Application.Main/Program.cs
+++ Sources/Application/Application.Main/Program.cs
@@ -1,19 +1,11 @@
+using Application.Main;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
-using Application.Main;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-// Aqui começa o truque:
-var baseElement = builder.HostEnvironment.BaseAddress;
-#if !DEBUG
-    baseElement = "https://celinhodaltro.github.io/Portifolio/";
-#endif
-builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
