4a56198ca95209bbfc4a1041385d8d954e1d2442
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-29 16:14:30 -0300

    alterar program

--- Sources/Application/Application.Main/Program.cs
+++ Sources/Application/Application.Main/Program.cs
@@ -5,16 +5,14 @@ using MudBlazor.Services;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-var baseElement = builder.HostEnvironment.BaseAddress;
-#if !DEBUG
-baseElement = "https://celinhodaltro.github.io/Portifolio/";
-#endif
-builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
-
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
+
+
+
+
+
