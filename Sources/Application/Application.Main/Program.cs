42c8b413011b8b88386b110c22d9ef204afb5a1c
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-04-29 11:18:23 -0300

    ajuste

--- Sources/Application/Application.Main/Program.cs
+++ Sources/Application/Application.Main/Program.cs
@@ -5,10 +5,17 @@ using MudBlazor.Services;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var baseElement = builder.HostEnvironment.BaseAddress;
+#if !DEBUG
+    baseElement = "https://celinhodaltro.github.io/Portifolio/";
+#endif
 builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseElement) });
 
+
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
