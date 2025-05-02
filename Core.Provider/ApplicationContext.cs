047160e14eb13b0c0cb0fe9251c8c278321e4894
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-05-02 11:19:26 -0300

    Criar provider

---
+++ Core.Provider/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Provider
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
