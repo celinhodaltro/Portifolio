4ff3f283ba2bb1aa4bad18a03d8439e0aab69b05
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-04 00:08:01 -0300

    Correção de erros

---
+++ Sources/Core/Core.Entities/Entities/Technology.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Core.Entities
+{
+    public class Technology
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public Color Color { get; set; }
+        public int YearsExperience { get; set; }
+        public string Description { get; set; }
+    }
+}
