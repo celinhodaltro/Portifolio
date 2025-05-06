d9af118cbf971907a799d756060730d53fe1596b
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-06 13:30:22 -0300

    Adicionar entidade de skill

---
+++ Sources/Core/Core.Entities/Entities/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Skill
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Level { get; set; } = 0;
+        public string Duration { get; set; } = "0 months";  // Adicionando a propriedade de duração
+    }
+
+}
