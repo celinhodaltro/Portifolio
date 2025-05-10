10d583499a8a011aa7ecf1c88b62e54bae09bda4
João Marcelo Daltro Marques <celinhodaltro@gmail.com>
2025-05-10 07:16:49 -0300

    Renomeia classe Projeto para Project e atualiza código
    As alterações incluem a renomeação da classe `Projeto` para `Project`, refletindo uma mudança na nomenclatura. Todas as referências à classe foram atualizadas, incluindo propriedades e métodos. O texto na interface foi modificado de "Meus Projetos" para "Meus Projects".
    
    Além disso, novos parâmetros foram adicionados ao construtor da classe `Project`, como `status`, e foram definidas propriedades públicas para `Nome`, `Descricao`, `Link`, `ImageUrl` e `Status`. O layout e a estrutura do componente foram mantidos, com ajustes para refletir as novas nomenclaturas e propriedades.
    
    Por fim, foram realizadas modificações nos dados binários do arquivo Vinculo.png, que podem afetar a compressão e resolução da imagem.

---
+++ Sources/Core/Core.Entities/Entities/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Project
+    {
+        public Project(string nome, string descricao, string? link = null, string? imageUrl = null, string status = "Concluído")
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Link = link;
+            ImageUrl = imageUrl;
+            Status = status;
+        }
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string? Link { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Status { get; set; }
+    }
+}
